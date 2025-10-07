2d5fead68744ed523fb38c418dc80f60b2e73de1
Nur Horiah <nurhoriah@gmail.com>
2025-10-07 18:28:39 +0700

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,17 @@
 const nextConfig = {
   output: 'export', // penting untuk GitHub Pages
   images: {
-    unoptimized: true,
+    unoptimized: true, // biar gambar bisa muncul di GitHub Pages
   },
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true, // lewati error linting saat build
   },
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true, // lewati error TypeScript saat build
   },
   basePath: '/Tipe-Klasifikasi-Karakter-Belajar', // Ganti sesuai nama repo kamu
-  assetPrefix: '/Tipe-Klasifikasi-Karakter-Belajar/', // biar gambar & CSS tampil di GitHub Pages
+  assetPrefix: '/Tipe-Klasifikasi-Karakter-Belajar/', // biar semua aset tampil di GitHub Pages
+  trailingSlash: true, // 🔥 tambahan penting biar semua halaman bisa diakses (tanpa 404)
 }
 
 export default nextConfig
